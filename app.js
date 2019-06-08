bf09c78c8126f274fd317ce0e71c45a8bb06f501
raye <38679090+nascenticity@users.noreply.github.com>
2019-06-08 00:58:39 -0500

    set up connection with sql server database

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const sqlDB = require('mssql');
+const config = require('./config.json');
+
+const connection = sqlDB.connect(config);
 
 // ROUTES - respond to requests for a given url with... something.
 app.get("/", (req, res)=>{
@@ -8,7 +12,13 @@ app.get("/", (req, res)=>{
 });
 app.get("/contacts", (req, res)=>{
     console.log("Getting contacts");
-    res.send("This will be replaced with data later"); 
+    sqlDB.connect(config, ()=>{
+        const request = new sqlDB.Request();
+        request.query("SELECT * FROM Contacts", (err, recordset)=>{
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset));
+        });
+    });
 })
 
 app.listen(1234,()=>{
